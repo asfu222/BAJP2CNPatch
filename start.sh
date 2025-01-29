1bce6ff073aec0eff72cac9ee888f7ccd6066231
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-01-28 19:57:38 -0500

    简化cmd与sh脚本

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --ignore-hosts $(hostname -I | awk '{print $1}')
+mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --allow-hosts prod-clientpatch.bluearchiveyostar.com:443
