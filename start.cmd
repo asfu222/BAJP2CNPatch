0800a89e5196b9e094a9e4a7cfc016c8b6d3b755
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-01-29 12:26:37 -0500

    修复不能连接到mitm.it的bug

--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --allow-hosts prod-clientpatch.bluearchiveyostar.com:443
+mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --allow-hosts prod-clientpatch.bluearchiveyostar.com:443 --allow-hosts mitm.it
