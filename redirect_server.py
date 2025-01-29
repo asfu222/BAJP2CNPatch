a7a8c2797b165be69de9210459fc269700a9b1f6
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-01-28 19:55:34 -0500

    修复资源服务器逻辑bug

--- redirect_server.py
+++ redirect_server.py
@@ -14,8 +14,9 @@ def get_local_ip():
 # 文件服务器
 FILE_SERVER_SCHEME = 'https' # 服务器网络协议（通常是http或https）
 # FILE_SERVER_SCHEME = 'http' 本地服务器
-FILE_SERVER_HOST = 'asfu222.github.io/BAJP2CNResources/assets/' # 服务器网址
+FILE_SERVER_HOST = 'asfu222.github.io' # 服务器网址
 # FILE_SERVER_HOST = get_local_ip() # 本地服务器
+FILE_SERVER_EXTRA_PATH = '/BAJP2CNResources/assets' # 附加路径，可不填
 FILE_SERVER_PORT = 443 # 服务器端口
 # FILE_SERVER_PORT = 8000 # 本地服务器
 
@@ -26,15 +27,17 @@ BA_FS_HOSTS = [
 
 def request(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_host in BA_FS_HOSTS:
-        if False: # 如果不知道BA版本号/服务器没有版本号，请把这个改成True。如果这个改成True，且作者/服务器主没更新latest资源包，会出现游戏内没有文字显现的bug
+        if False: # 如果不知道BA版本号/服务器没有版本号，请把这个改成True。<请注意> 如果这个改成True，且作者/服务器主没更新latest资源包，会出现游戏内没有文字显现的bug （汉化落后与日服最新资源包）
             flow.request.path = '/latest/' + '/'.join(flow.request.path.split('/')[2:])
-        file_server_url = f'http://{FILE_SERVER_HOST}:{FILE_SERVER_PORT}{flow.request.path}'
+        flow.request.path = FILE_SERVER_EXTRA_PATH + flow.request.path
+        file_server_url = f'{FILE_SERVER_SCHEME}://{FILE_SERVER_HOST}:{FILE_SERVER_PORT}{flow.request.path}'
         try:
-            response = requests.head(file_server_url, timeout=5)
+            response = requests.head(file_server_url, allow_redirects=True, timeout=5)
             if response.status_code == 200:
                 # 拦截官方资源服务器下载请求，并修改至上面的文件服务器网址
                 flow.request.scheme = FILE_SERVER_SCHEME
                 flow.request.host = FILE_SERVER_HOST
                 flow.request.port = FILE_SERVER_PORT
-        except requests.RequestException:
+        except requests.RequestException as e:
+            print(e)
             pass
