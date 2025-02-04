1cbf74571603a259f9a3c31db0cbb9a05e192a40
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-02-03 22:57:23 -0500

    优化下载速度

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --allow-hosts prod-clientpatch.bluearchiveyostar.com:443 --allow-hosts mitm.it
+mitmweb -m wireguard --no-http2 -s redirect_server.py --set termlog_verbosity=warn --allow-hosts prod-clientpatch.bluearchiveyostar.com:443 --allow-hosts mitm.it --set stream_large_bodies=2000
